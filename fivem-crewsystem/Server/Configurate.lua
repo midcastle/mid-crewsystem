6b412a5b28220036bdcab21d4b8cc3b63ce9a734
citydesk <97290209+citydesk@users.noreply.github.com>
2022-01-08 13:23:13 +0800

    Add files via upload

---
+++ fivem-crewsystem/Server/Configurate.lua
@@ -0,0 +1,78 @@
+ASD = nil
+
+CreateThread(function()
+    while not ASD do
+        TriggerEvent('esx:getSharedObject', function(obj) ASD = obj end)
+        Wait(500)
+    end
+
+	if enable_rank then
+		MySQL.Async.execute('CREATE TABLE IF NOT EXISTS ranking_crew(id int AUTO_INCREMENT, name varchar(100), created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, members int DEFAULT 1, kills int, PRIMARY KEY(id))')
+	end
+
+	MySQL.Async.execute('CREATE TABLE IF NOT EXISTS crew(id int AUTO_INCREMENT, name varchar(100), identifier varchar(100), grade varchar(50), PRIMARY KEY(id))',{},
+	function()
+		MySQL.Async.fetchAll('SELECT * FROM crew', {},
+		function(result)
+			for i,k in pairs(result) do
+				local found = false
+
+				for _, crew in pairs(crews) do
+					if crew.name == k.name then
+						table.insert(crew.players, {
+							identifier = k.identifier,
+							source = -1,
+							grade = k.grade
+						})
+
+						found = true
+					end
+				end
+
+				if not found then
+					table.insert(crews, {
+						name = k.name,
+						players = {
+							{identifier = k.identifier, source = -1, grade = k.grade}
+						}
+					})
+				end
+			end
+		end)
+	end)
+end)
+
+function getIdentifier(source)
+    local xPlayer = ASD.GetPlayerFromId(source)
+	if xPlayer then
+		local identifier = xPlayer.getIdentifier()
+
+		return identifier
+	end
+
+	return nil
+end
+
+function GetPlayerFromIdentifier(identifier)
+    return ASD.GetPlayerFromIdentifier(identifier)
+end
+
+function showNotification(source, message)
+    TriggerClientEvent('asd:showNotification', source, message)
+    TriggerClientEvent('chat:addMessage', source, { args = { message }})
+end
+
+function GetPlayersOnline()
+    return ASD.GetPlayers()
+end
+
+function getName(source)
+    local xPlayer = ASD.GetPlayerFromId(source)
+
+	if xPlayer then
+		local name = xPlayer.getName()
+		return name
+	end
+
+	return GetPlayerName(source)
+end
